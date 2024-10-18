a3cedece9a43fa0cf4f1c5f736f482cae5fad627
Mark Maslakowski <mmaslak@unknown72eeceb4e9ba.attlocal.net>
2024-10-17 21:05:43 -0500

    WAR changes

--- src/main/java/com/blessingtree/BlessingTreeApplication.java
+++ src/main/java/com/blessingtree/BlessingTreeApplication.java
@@ -2,9 +2,9 @@ package com.blessingtree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class BlessingTreeApplication {
+public class BlessingTreeApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(BlessingTreeApplication.class, args);
