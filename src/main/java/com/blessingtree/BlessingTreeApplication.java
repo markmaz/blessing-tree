78872f170895ccfab1371f1335242fdb00437312
Mark Maslakowski <mmaslak@unknown72eeceb4e9ba.attlocal.net>
2024-10-20 10:14:34 -0500

    Added spring config

--- src/main/java/com/blessingtree/BlessingTreeApplication.java
+++ src/main/java/com/blessingtree/BlessingTreeApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class BlessingTreeApplication extends SpringBootServletInitializer{
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BlessingTreeApplication.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(BlessingTreeApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlessingTreeApplication.class);
+    }
 }
