b2a44828d32791006fbf1221202ee3913b94f833
Mark Maslakowski <mmaslak@Marks-MacBook-Pro.local>
2024-11-16 00:20:13 -0600

    Debug

--- src/main/java/com/blessingtree/service/Parser.java
+++ src/main/java/com/blessingtree/service/Parser.java
@@ -8,13 +8,11 @@ import com.blessingtree.repository.SponsorRepository;
 import org.apache.http.HttpStatus;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class EmailService extends BaseService{
+public class Parser {
     public static final int FIRST_NAME = 0;
     public static final int LAST_NAME = 1;
     public static final int ADDRESS = 2;
@@ -30,7 +28,7 @@ public class EmailService extends BaseService{
     public static final int GENDER_PREFERENCE = 12;
     public static final int HEAR_ABOUT_US = 13;
     public static final int WANT_TO_VOLUNTEER = 14;
-    private final SponsorRepository sponsorRepository;
+
     private static final String[] EMAIL_VALUES = {"*First Name:*", "*Last Name:*", "*Address:*", "*City:*", "*State:*",
             "*Zip:*", "*Email:*", "*Phone Number:*", "*Best Time To Call:*",
             "*Have you sponsored a child/children through The Blessing Tree Program\n" +
@@ -46,19 +44,42 @@ public class EmailService extends BaseService{
                     "Services?:*"};
     private static final String EMAIL_SPLIT = "\\*Blessing Tree Sponsor <https://st-mm.com/blessing-tree-sponsor-survey>\\*";
 
-    public EmailService(@Autowired ModelMapper mapper,
-                        @Autowired SponsorRepository sponsorRepository){
-        super(mapper);
-        this.sponsorRepository = sponsorRepository;
+    public static void main(String[] args){
+        EmailDTO emailDTO = getEmail();
+        parseBodyAndCreateSponsor(emailDTO);
     }
 
-    public int parseBodyAndCreateSponsor(EmailDTO emailDTO){
-        String[] body = emailDTO.getBody().split(EMAIL_SPLIT);
-        String heads = body[1].trim().replaceAll("\r", "");
+    public static EmailDTO getEmail(){
+        EmailDTO emailDTO = new EmailDTO();
+        emailDTO.setBody("*First Name:* Terry\n" +
+                "*Last Name:* Mares\n" +
+                "*Address:* 4862 gypsy forest dr\n" +
+                "*City:* Humble\n" +
+                "*State:* TX\n" +
+                "*Zip:* 77346\n" +
+                "*Email:* tmares.3157@gmail.com\n" +
+                "*Phone Number:* 8327900837\n" +
+                "*Best Time To Call:* Any time\n" +
+                "*Have you sponsored a child/children through The Blessing Tree Program\n" +
+                "before?:* YES\n" +
+                "*This year, each parent has indicated one wish / per child. How many\n" +
+                "children would you like to sponsor this year?:* 1\n" +
+                "*Do you have an age preference for the child that you would like to\n" +
+                "sponsor?:* 6 -12 years\n" +
+                "*Do you have a preference on the gender of the child that you would like to\n" +
+                "sponsor?:* Either\n" +
+                "*How did you hear about St. Mary Magdalene’s Blessing Tree Program?:*\n" +
+                "*Would you be interested in becoming a volunteer of Magdalene House Social\n" +
+                "Services?:* Not at this time");
+        return emailDTO;
+    }
+    public static int parseBodyAndCreateSponsor(EmailDTO emailDTO){
+        String heads = emailDTO.getBody();
+        //String heads = body[1].trim();
 
         List<String> entityValues = new ArrayList<>();
 
-        for(int i = 0; i < EMAIL_VALUES.length; i++){
+        for(int i = 0; i < EMAIL_VALUES.length ; i++){
             String out;
 
             if (i < EMAIL_VALUES.length - 1){
@@ -74,11 +95,12 @@ public class EmailService extends BaseService{
             }
         }
 
-        sponsorRepository.save(mapSponsor(entityValues));
+        Sponsor sponsor = mapSponsor(entityValues);
+        System.out.println(sponsor);
         return HttpStatus.SC_OK;
     }
 
-    private Sponsor mapSponsor(List<String> entityValues) {
+    private static Sponsor mapSponsor(List<String> entityValues) {
         Sponsor sponsor = new Sponsor();
         sponsor.setFirstName(entityValues.get(FIRST_NAME));
         sponsor.setLastName(entityValues.get(LAST_NAME));
@@ -107,7 +129,7 @@ public class EmailService extends BaseService{
         return sponsor;
     }
 
-    private Boolean convertToBool(String s) {
+    private static Boolean convertToBool(String s) {
         if(s.equalsIgnoreCase("yes") || s.equalsIgnoreCase("y")
                 || s.equalsIgnoreCase("true")){
             return true;
